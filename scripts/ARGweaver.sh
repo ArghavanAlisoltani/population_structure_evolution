b3e963f244744ebbe9c8f01a968e00009e8a9b0a
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-14 11:48:54 -0500

    Update ARGweaver.sh

--- scripts/ARGweaver.sh
+++ scripts/ARGweaver.sh
@@ -1,1 +1,8 @@
+# 1) ARGweaver MCMC on the region
+arg-sample \
+  --vcf "path/to/s100_All_1a1b_renamed.filtered.vcf.gz" \
+  --region "scaffold_1a:750000001-900000000" \
+  -N 10000 --randseed 1753216431 -r 1.6e-8 -m 1.8e-8 \
+  --ntimes 20 --maxtime 2e5 -c 20 -n 50 \
+  -o "/outargs_scaffold_1a_750000001_900000000"
 
