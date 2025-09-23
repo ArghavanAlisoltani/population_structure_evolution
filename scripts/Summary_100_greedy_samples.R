38b16c6371b771d472ce5207b7cbe86d3a986ef3
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-22 19:19:08 -0500

    Update Summary_100_greedy_samples.R

--- scripts/Summary_100_greedy_samples.R
+++ scripts/Summary_100_greedy_samples.R
@@ -6,6 +6,7 @@ library(ggplot2)
 setwd("~/Desktop/OSU_projects/conifers/LP/FSTcalc/provenances/100_greedy/fst_per_site")
 
 # === Inputs ===
+
 # Vector of vcftools per-site FST files (one per comparison)
 files <- c(
   "PROC3_vs_PROC4.weir.fst",
