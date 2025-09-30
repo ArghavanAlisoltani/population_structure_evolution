e4a58bc51138573b67483c5b61d0548bab841cea
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-30 17:00:05 -0500

    Update vcf_shifted_position_breakpoints_prep.sh

--- scripts/argweaver/vcf_shifted_position_breakpoints_prep.sh
+++ scripts/argweaver/vcf_shifted_position_breakpoints_prep.sh
@@ -8,6 +8,6 @@ bcftools sort -Oz -o split_poly_s100_scaffolds.sorted.vcf.gz \
 
 # 1) CSI indexes (not TBI)
 module load bcftools/1.8
-bcftools index -c -f split_poly_s100_scaffolds.vcf.gz
+bcftools index -c -f split_poly_s100_scaffolds.sorted.vcf.gz
 #or
-#bcftools index -c split_poly_s100_scaffolds.vcf.gz
+#bcftools index -c split_poly_s100_scaffolds.sorted.vcf.gz
