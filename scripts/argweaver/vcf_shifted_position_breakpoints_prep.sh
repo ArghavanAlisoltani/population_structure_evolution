f68161cc11a74a76ad2fc0b968459c04ca9a57f1
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-30 16:58:29 -0500

    Update vcf_shifted_position_breakpoints_prep.sh

--- scripts/argweaver/vcf_shifted_position_breakpoints_prep.sh
+++ scripts/argweaver/vcf_shifted_position_breakpoints_prep.sh
@@ -1,13 +1,13 @@
 module load bcftools/1.8
-bcftools view -Oz -o renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz \
-  renamed_1ab_samples_1489_merged_sorted_tworules.vcf
+bcftools view -Oz -o split_poly_s100_scaffolds.vcf.gz \
+  split_poly_s100_scaffolds.vcf
 
 # (optional but smart) ensure position-sorted before indexing
-bcftools sort -Oz -o renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz \
-  renamed_1ab_samples_1489_merged_sorted_tworules.vcf
+bcftools sort -Oz -o split_poly_s100_scaffolds.vcf.gz \
+ split_poly_s100_scaffolds.vcf.vcf
 
 # 1) CSI indexes (not TBI)
 module load bcftools/1.8
-bcftools index -c -f renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz
+bcftools index -c -f split_poly_s100_scaffolds.vcf.gz
 #or
-bcftools index -c renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz
+#bcftools index -c split_poly_s100_scaffolds.vcf.gz
