688f0efa8d3641bbe4700f2fde17af604ff412c8
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-10-11 22:58:29 -0500

    Update region_sanity_check.sh

--- scripts/argweaver/region_sanity_check.sh
+++ scripts/argweaver/region_sanity_check.sh
@@ -6,6 +6,5 @@ bcftools view "/scratch/arghavan/LP/subsampling/split_poly_s100_scaffolds.sorted
 
 bcftools view "/scratch/arghavan/LP/subsampling/split_poly_s100_scaffolds.sorted.vcf.gz" 
 
-bcftools view "/scratch/arghavan/LP/subsampling/poly_s100_All_1a1b_renamed.vcf.gz" "scaffold_2:300000000-350000000" | wc -l
+bcftools view "/scratch/arghavan/LP/subsampling/poly_s100_All_1a1b_renamed.vcf.gz" "scaffold_2:340000001-450000000" | wc -l
 
-scaffold_2:340000001-450000000
