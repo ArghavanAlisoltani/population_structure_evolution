70ef83a190a7a72e3bc9841423700c2b5323701a
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-10-11 22:29:29 -0500

    Update and rename region_sanity_check to region_sanity_check.sh

--- scripts/argweaver/region_sanity_check.sh
+++ scripts/argweaver/region_sanity_check.sh
@@ -1,1 +1,3 @@
 bcftools view "/scratch/arghavan/LP/subsampling/poly_s100_All_1a1b_renamed.vcf.gz" "scaffold_2:300000000-350000000" | wc -l
+
+bcftools view "/scratch/arghavan/LP/subsampling/split_poly_s100_scaffolds.sorted.vcf.gz" "scaffold_2b:1-40000000" | wc -l
