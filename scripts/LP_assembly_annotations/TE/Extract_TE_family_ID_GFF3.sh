6a6557f50495068c973aaae818ac6154c76c20e4
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-10-16 12:56:15 -0500

    Add script to extract TE family IDs from GFF3
    Extract relevant TE information from GFF3 file.

---
+++ scripts/LP_assembly_annotations/TE/Extract_TE_family_ID_GFF3.sh
@@ -0,0 +1,7 @@
+# GFF3 columns: seqid source feature start end score strand phase attributes
+# Pull seqid, start, end, feature, strand, and TE Name= (family ID)
+awk 'BEGIN{OFS="\t"} !/^#/{
+  name="NA";
+  if (match($9, /(^|;)Name=([^;]+)/, a)) name=a[2];
+  print $1,$4,$5,$3,$7,name
+}' your.edta.gff3 > gff_instances.tsv
