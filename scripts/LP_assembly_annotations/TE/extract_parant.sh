9f1dd65e1d329089a7ac66f8c3add4d9cd2f4499
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-10-16 13:19:00 -0500

    Update extract_parant.sh

--- scripts/LP_assembly_annotations/TE/extract_parant.sh
+++ scripts/LP_assembly_annotations/TE/extract_parant.sh
@@ -10,5 +10,5 @@ awk -F'\t' 'BEGIN{OFS="\t"} !/^#/ && $3=="long_terminal_repeat"{
   par="."; if(match($9,/Parent=([^;]+)/,p)) par=p[1];
   id=".";  if(match($9,/ID=([^;]+)/,m)) id=m[1];
   print $1,$4,$5,$7,par,id
-}' your.edta.gff3 > ltrs.tsv
+}' TEanno.cds.gff3 > ltrs.tsv
 # cols: seqid  start  end  strand  parent_id  ltr_id
