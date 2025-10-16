cff2aef0a36b3c426310052d8288c864a7b8c610
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-10-16 13:22:23 -0500

    Update extract_parant.sh

--- scripts/LP_assembly_annotations/TE/extract_parant.sh
+++ scripts/LP_assembly_annotations/TE/extract_parant.sh
@@ -1,14 +1,24 @@
 # Extract parents
-awk -F'\t' 'BEGIN{OFS="\t"} !/^#/ && $3=="LTR_retrotransposon"{
-  id="."; if(match($9,/ID=([^;]+)/,m)) id=m[1];
+awk -F'\t' 'BEGIN{OFS="\t"}
+$0 !~ /^#/ && $3=="LTR_retrotransposon"{
+  id=".";
+  n=split($9,a,";");
+  for(i=1;i<=n;i++){
+    split(a[i],b,"=");
+    if(b[1]=="ID"){ id=b[2]; }
+  }
   print $1,$4,$5,$7,id
 }' TEanno.cds.gff3 > parents.tsv
-# cols: seqid  start  end  strand  parent_id
 
-# Extract child LTRs
-awk -F'\t' 'BEGIN{OFS="\t"} !/^#/ && $3=="long_terminal_repeat"{
-  par="."; if(match($9,/Parent=([^;]+)/,p)) par=p[1];
-  id=".";  if(match($9,/ID=([^;]+)/,m)) id=m[1];
-  print $1,$4,$5,$7,par,id
+awk -F'\t' 'BEGIN{OFS="\t"}
+$0 !~ /^#/ && $3=="long_terminal_repeat"{
+  par="."; cid=".";
+  n=split($9,a,";");
+  for(i=1;i<=n;i++){
+    split(a[i],b,"=");
+    if(b[1]=="Parent"){ par=b[2]; }
+    else if(b[1]=="ID"){ cid=b[2]; }
+  }
+  print $1,$4,$5,$7,par,cid
 }' TEanno.cds.gff3 > ltrs.tsv
-# cols: seqid  start  end  strand  parent_id  ltr_id
+
