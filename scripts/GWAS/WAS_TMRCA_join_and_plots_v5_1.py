1f2cee143a464d6775a7bf299ef948a6bc9c2b2e
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-12-03 18:14:53 -0600

    Update WAS_TMRCA_join_and_plots_v5_1.py

--- scripts/GWAS/WAS_TMRCA_join_and_plots_v5_1.py
+++ scripts/GWAS/WAS_TMRCA_join_and_plots_v5_1.py
@@ -10,7 +10,7 @@ from matplotlib import cm
 Run examples
 cd /Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/MTAG_NOV_2025
 
-python GWAS_TMRCA_join_and_plots_v5.1.py \
+python GWAS_TMRCA_join_and_plots_v5_1.py \
   --gwas "/Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/MTAG_NOV_2025/To_merge_with_TMRCA_MTAG_SIgnificant_sumstat.txt" \
   --tmrca "/Users/aria/Desktop/OSU_projects/conifers/LP/ARGweaver/NOV_27_2025/all_tmrca_as_scaffold1.tsv" \
   --outdir "gwas_MTAG_trait_tmrca_results_v5" \
@@ -21,7 +21,7 @@ python GWAS_TMRCA_join_and_plots_v5.1.py \
 
 cd /Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/sumstat_files
 
-python GWAS_TMRCA_join_and_plots_v5.1.py \
+python GWAS_TMRCA_join_and_plots_v5_1.py \
   --gwas "/Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/sumstat_files/combined_pval_maf_hits/combined_hits_full_merge_with_tmrca.txt" \
   --tmrca "/Users/aria/Desktop/OSU_projects/conifers/LP/ARGweaver/NOV_27_2025/all_tmrca_as_scaffold1.tsv" \
   --outdir "gwas_single_trait_tmrca_results_v5" \
