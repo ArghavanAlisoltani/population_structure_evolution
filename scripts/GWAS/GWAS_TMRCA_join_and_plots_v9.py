e47457fbd4a3e9b129740f8eb66a039b48b08d53
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-12-04 18:04:28 -0600

    Update GWAS_TMRCA_join_and_plots_v9.py

--- scripts/GWAS/GWAS_TMRCA_join_and_plots_v9.py
+++ scripts/GWAS/GWAS_TMRCA_join_and_plots_v9.py
@@ -31,7 +31,7 @@ cd /Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/MTAG_NOV_2025
 python GWAS_TMRCA_join_and_plots_v9.py \
   --gwas "/Users/aria/Desktop/OSU_projects/conifers/LP/Soms_shared/sumstat_files/combined_pval_maf_hits/combined_hits_full_merge_with_tmrca.txt" \
   --tmrca "/Users/aria/Desktop/OSU_projects/conifers/LP/ARGweaver/NOV_27_2025/all_tmrca_as_scaffold1.tsv" \
-  --outdir "gwas_MTAG_trait_tmrca_results_v9" \
+  --outdir "gwas_single_trait_tmrca_results_v9" \
   --regline --show-lm --show-r --show-p \
   --stats-outside --stats-panel --stats-position top --stats-panel-height 1.6 \
   --min-n-trait 5 \
