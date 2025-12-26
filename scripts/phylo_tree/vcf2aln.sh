97e54a932ac1176e6adb76d4b0ca5bfba20f18e2
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-12-25 22:33:18 -0600

    Change output filename for filtered VCF

--- scripts/phylo_tree/vcf2aln.sh
+++ scripts/phylo_tree/vcf2aln.sh
@@ -3,6 +3,8 @@ bcftools view \
   -m2 -M2 \
   -v snps \
   -Oz \
-  -o filtered.vcf.gz \
+  -o filtered_whole_Esteban_imputed.vcf.gz \
   Imputed_whole_panel_Esteban_Soms_shared.vcf
 
+
+#Index it:
