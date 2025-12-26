82914add29ab7474658483cdcf8a018113a7a2ab
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-12-25 22:41:38 -0600

    Update comments for clarity in vcf2aln.sh

--- scripts/phylo_tree/vcf2aln.sh
+++ scripts/phylo_tree/vcf2aln.sh
@@ -1,4 +1,4 @@
-#Pre-filter VCF (biallelic SNPs only)
+#1-Pre-filter VCF (biallelic SNPs only)
 bcftools view \
   -m2 -M2 \
   -v snps \
@@ -7,10 +7,10 @@ bcftools view \
   Imputed_whole_panel_Esteban_Soms_shared.vcf
 
 
-# Index it:
+#2- Index it:
 bcftools index filtered_whole_Esteban_imputed.vcf.gz 
 
-# Convert VCF → PLINK (keeps sample order stable)
+#3- Convert VCF → PLINK (keeps sample order stable)
 plink2 \                      
   --vcf filtered_whole_Esteban_imputed.vcf.gz \
   --allow-extra-chr \
@@ -19,7 +19,7 @@ plink2 \
   --make-bed \
   --out panel
 
-# Optional but STRONGLY recommended filtering two steps
+# 4-Optional but STRONGLY recommended filtering two steps
 # This avoids pathological trees and speeds everything up.plink2 \
   --bfile panel  --allow-extra-chr \
   --maf 0.05 \
@@ -34,5 +34,14 @@ plink2 \
   --make-bed \
   --out panel_pruned
 
+# 5- PLINK2 → PHYLIP alignment for iqtree2
+  plink2 \
+  --bfile panel_pruned --allow-extra-chr \
+  --export phylip \
+  --out alignment
 
+# 6- PHYLIP → FASTA alignment for fasttree
+#pip install seqmagick
+
+seqmagick convert alignment.phy alignment.fasta
 
