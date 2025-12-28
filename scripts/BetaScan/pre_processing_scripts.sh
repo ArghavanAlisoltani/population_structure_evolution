8cc367e7a0b5130d7355e02bf39d1781942e3e61
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-12-27 23:15:38 -0600

    Update pre_processing_scripts.sh

--- scripts/BetaScan/pre_processing_scripts.sh
+++ scripts/BetaScan/pre_processing_scripts.sh
@@ -1,1 +1,4 @@
-
+#
+bcftools view -m2 -M2 -v snps -r scaffold_4 Imputed_whole_panel_Esteban_Soms_shared.vcf.gz \
+| bcftools +fill-tags -Oz -o scaffold_4.biallelic.tags.vcf.gz -- -t AN,AC,AF
+bcftools index -f scaffold_4.biallelic.tags.vcf.gz
